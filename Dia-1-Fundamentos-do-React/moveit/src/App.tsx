6bcbb3c798c389fed500ba096dce164c0907dabe
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-22 22:36:00 -0300

    Definido as cores da aplicação

--- Dia-1-Fundamentos-do-React/moveit/src/App.tsx
+++ Dia-1-Fundamentos-do-React/moveit/src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import { Button } from './components/Button';
-
 function App() {
   return (
     <div className="App">
-      <Button color="red">Vermelho</Button>
-      <Button color="yellow">Amarelo</Button>
-      <Button color="blue">Azul</Button>
+
     </div>
   );
 }
