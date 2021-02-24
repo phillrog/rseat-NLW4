2b71d2b1400ee4e3b204e77205ebc6d1f80d82c3
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-23 22:19:36 -0300

    Foi criado o esqueleto do componente contador

---
+++ Dia-2-Desvendando-o-nextjs/moveit-next/src/components/Countdown.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/components/Countdown.module.css";
+
+export function Countdown() {
+    return (
+        <div className={styles.countdownContainer}>
+
+        </div>
+    )
+}
