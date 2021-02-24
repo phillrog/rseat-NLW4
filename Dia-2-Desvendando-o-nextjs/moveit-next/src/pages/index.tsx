295f39e2d92be41874024a5728d11aee08d6949e
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-23 21:55:27 -0300

    Foi criado o home css

--- Dia-2-Desvendando-o-nextjs/moveit-next/src/pages/index.tsx
+++ Dia-2-Desvendando-o-nextjs/moveit-next/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import Head from 'next/head'
 import { Profile } from "../components/Perfil";
+import styles from '../styles/components/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
 
       <ExperienceBar></ExperienceBar>
 
