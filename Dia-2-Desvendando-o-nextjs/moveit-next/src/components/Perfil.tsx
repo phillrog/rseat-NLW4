ddb232c81248c6e8f413267551bf1fdcfeb3a592
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-23 21:08:40 -0300

    Foi criado a casca do componente Perfil

---
+++ Dia-2-Desvendando-o-nextjs/moveit-next/src/components/Perfil.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/phillrog.png" alt="Phill" />
+            <div>
+                <strong>Phillipe Roger Souza</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
--- Dia-2-Desvendando-o-nextjs/moveit-next/src/pages/index.tsx
+++ Dia-2-Desvendando-o-nextjs/moveit-next/src/pages/index.tsx
@@ -1,11 +1,21 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import Head from 'next/head'
+import { Profile } from "../components/Perfil";
 
 export default function Home() {
   return (
     <div className="container">
 
       <ExperienceBar></ExperienceBar>
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
