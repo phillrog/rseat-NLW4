bab5542131d497b94a723ecd061f605b4c30c31c
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-24 22:09:36 -0300

    Zerando o timer

--- Dia-3-Contextos-e-desafios/moveit-next/src/components/Countdown.tsx
+++ Dia-3-Contextos-e-desafios/moveit-next/src/components/Countdown.tsx
@@ -7,6 +7,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function Countdown() {
     const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -28,6 +29,9 @@ export function Countdown() {
             countdownTimeout = setTimeout(() => {
                 setTime(time - 1);
             }, 1000);
+        } else if (isActive && time === 0) {
+            setHasFinished(true);
+            setIsActive(false);
         }
     }, [isActive, time])
 
