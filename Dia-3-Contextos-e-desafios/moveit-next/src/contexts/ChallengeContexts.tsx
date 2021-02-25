52d71dce9f81867b97a57c2a83505ae5b9ca91bd
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-24 23:02:11 -0300

    Foi implementado o start do desafio

--- Dia-3-Contextos-e-desafios/moveit-next/src/contexts/ChallengeContexts.tsx
+++ Dia-3-Contextos-e-desafios/moveit-next/src/contexts/ChallengeContexts.tsx
@@ -9,8 +9,15 @@ interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
+}
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
@@ -19,13 +26,17 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
+    const [activeChallenge, setActiveChallenge] = useState(null);
 
     function levelUp() {
         setLevel(level + 1);
     }
 
     function startNewChallenge() {
-        console.log()
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
     }
 
     return (
@@ -35,7 +46,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 currentExperience,
                 challengesCompleted,
                 levelUp,
-                startNewChallenge
+                startNewChallenge,
+                activeChallenge
             }}
         >
             {children}
