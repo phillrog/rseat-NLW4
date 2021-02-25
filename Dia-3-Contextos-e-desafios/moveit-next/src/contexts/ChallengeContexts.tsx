f9b75cbaa571f79cbdd22224b90128865715bdb9
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-24 22:47:35 -0300

    Foi configurado o contexto de forma inicial

--- Dia-3-Contextos-e-desafios/moveit-next/src/contexts/ChallengeContexts.tsx
+++ Dia-3-Contextos-e-desafios/moveit-next/src/contexts/ChallengeContexts.tsx
@@ -1,3 +1,21 @@
-import { createContext } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 
 export const ChallengesContext = createContext({});
+
+interface ChallengesProviderProps {
+    children: ReactNode;
+}
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+    const [level, setLevel] = useState(1);
+
+    return (
+        <ChallengesContext.Provider
+            value={{
+                level,
+            }}
+        >
+            {children}
+        </ChallengesContext.Provider>
+    );
+}
--- Dia-3-Contextos-e-desafios/moveit-next/src/pages/_app.tsx
+++ Dia-3-Contextos-e-desafios/moveit-next/src/pages/_app.tsx
@@ -1,7 +1,17 @@
+
+import React from 'react';
+import {
+  ChallengesProvider,
+} from '../contexts/ChallengeContexts';
+
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />;
+    </ChallengesProvider>
+  );
 }
 
 export default MyApp
