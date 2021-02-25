e8c8ddae81d3f069b895d6695f91a04fcd479e39
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-25 19:52:18 -0300

    Foi implementado o level no perfil

--- Dia-4-Unindo-toda-aplicacao/moveit-next/src/components/Perfil.tsx
+++ Dia-4-Unindo-toda-aplicacao/moveit-next/src/components/Perfil.tsx
@@ -1,12 +1,15 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengeContexts";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/phillrog.png" alt="Phill" />
             <div>
                 <strong>Phillipe Roger Souza</strong>
-                <p><img src="icons/level.svg" alt="Level" />Level 1 </p>
+                <p><img src="icons/level.svg" alt="Level" />Level {level} </p>
             </div>
         </div>
     );
