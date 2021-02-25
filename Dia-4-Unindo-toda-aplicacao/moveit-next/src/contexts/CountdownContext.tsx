da9ca2e2a9db00ee27303cede389eeb04c6bcc75
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-25 20:25:53 -0300

    Foi resetado o tempo do contador ao falhar

--- Dia-4-Unindo-toda-aplicacao/moveit-next/src/contexts/CountdownContext.tsx
+++ Dia-4-Unindo-toda-aplicacao/moveit-next/src/contexts/CountdownContext.tsx
@@ -34,7 +34,8 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     function resetCountdown() {
         clearTimeout(countdownTimeout);
         setIsActive(false);
-        setTime(25 * 60);
+        setTime(0.1 * 60);
+        setHasFinished(false);
     }
 
     useEffect(() => {
