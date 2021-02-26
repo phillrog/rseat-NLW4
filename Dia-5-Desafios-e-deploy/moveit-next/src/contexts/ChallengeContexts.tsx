3441a64d505df5951056924f4b39661b46049aab
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-26 20:53:31 -0300

    Foi implementado os valores do cookie

--- Dia-5-Desafios-e-deploy/moveit-next/src/contexts/ChallengeContexts.tsx
+++ Dia-5-Desafios-e-deploy/moveit-next/src/contexts/ChallengeContexts.tsx
@@ -29,10 +29,12 @@ interface Challenge {
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(30);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ children,
+    ...rest
+}: ChallengesProviderProps) {
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
     const [activeChallenge, setActiveChallenge] = useState(null);
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
