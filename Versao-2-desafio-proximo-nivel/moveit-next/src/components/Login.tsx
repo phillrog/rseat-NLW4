285c70fcfc705c6e0bfd408293080d0ea733205c
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-27 19:00:53 -0300

    Foi implementado o layout e a estilização

--- Versao-2-desafio-proximo-nivel/moveit-next/src/components/Login.tsx
+++ Versao-2-desafio-proximo-nivel/moveit-next/src/components/Login.tsx
@@ -2,14 +2,33 @@ import styles from '../styles/components/Login.module.css';
 
 export function Login() {
     return (
-        <section className={styles.containerLogin}>
-            <section >
-                <div className={styles.simbolo}></div>
-            </section>
-            <section >
-                <img src="/logo-full.svg" />
-                <p>Bem  vindo</p>
-            </section>
-        </section>
+        <div className={styles.container}>
+            <div className={styles.marcadagua}>
+                <div className={styles.linha1}></div>
+                <div className={styles.linha2}></div>
+                <div className={styles.linha3}></div>
+            </div>
+            <div className={styles.v149854_105}>
+
+                <img src="icons/logo.svg" />
+
+            </div>
+            <span className={styles.bemvindo}>Bem-vindo</span>
+            <div className={styles.github}>
+                <img src="icons/github.svg" />
+                <span className={styles.facalogin}>Faça login com seu Github
+            para começar</span>
+            </div>
+            <div className={styles.boxUsername}>
+                <span className={styles.username}>
+                    <input type="text" placeholder="Digite seu username" className={styles.username} />
+                </span>
+            </div>
+            <div className={styles.logar}>
+                <button type="button" className={styles.flecha}>
+                    <img src="icons/flecha.svg" />
+                </button>
+            </div>
+        </div>
     );
 }
